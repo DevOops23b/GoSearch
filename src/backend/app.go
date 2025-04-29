0ca17ea110a469a49279c559e6ce268ae48b5b4a
CVS2003 <clara.vej@outlook.com>
2025-04-29 21:55:21 +0200

    Minor change in app.go

--- src/backend/app.go
+++ src/backend/app.go
@@ -1235,6 +1235,6 @@ func main() {
 
 	fmt.Println("Server running on http://localhost:8080")
 	//Starter serveren.
-	log.Fatal(http.ListenAndServe(":8081", r))
+	log.Fatal(http.ListenAndServe("0.0.0.0:8081", r))
 
 }
